87a33638b9305124fe0c26fcf48142aa3fa7fb0d
e <yuan491187718@gmail.com>
2019-09-14 13:38:10 +0800

    update

--- src/main/java/leetcode/求x的n次方.java
+++ src/main/java/leetcode/求x的n次方.java
@@ -1,76 +1,61 @@
 package leetcode;
 
 public class 求x的n次方 {
-    public double myPow(double x, int n) {
-        if (n == 0) {
-            return 1;
-        }
-        double result = myPow(x, n/2);
-        if ((n & 1) == 1) {
-            return result * result;
-        } else if ((n & 1) == 0) {
-            return result * result * x;
-        }
-        return result * result / x;
-    }
 
-
-    public  double myPow2(double x, int n) {
+    public  double myPow(double x, int n) {
         if (Double.compare(x, 0) == 0 && n < 0) {
             // 整数位0，指数为负数的情况
             throw new RuntimeException("非法输入，除数不能为0");
         }
-        long absN = n;
-        if (n < 0) {
+        long N = n; // 防止溢出
+        if (N < 0) {
             // 负数
-            absN = -absN;
-            return 1.0 / PowerWithUnsigned2(x, absN);
+            return 1.0 / PowerWithUnsigned2(x, -N);
         }
         // 正数
-        return PowerWithUnsigned2(x, absN);
+        return PowerWithUnsigned2(x, N);
     }
-
     /**
      * 递归 O(lgn)
      * a^n =
      * (a^n/2)*(a^n/2) n为偶数
      * a^(n-1)/2 * a^(n-1)/2 * a  n为奇数
-     * @param base
-     * @param exponent
+     * @param x
+     * @param n
      * @return
      */
-    private double PowerWithUnsigned(double base, long exponent) {
+    private double PowerWithUnsigned(double x, long n) {
         // 边界
-        if (exponent == 0) {
+        if (n == 0) {
             return 1;
         }
-        if (exponent == 1) {
-            return base;
+        if (n == 1) {
+            return x;
         }
         // exponent >> 1 代替除以2
-        double result = PowerWithUnsigned(base, exponent >> 1);
+        double result = PowerWithUnsigned(x, n >> 1);
         result *= result;
-        if ((exponent & 1) == 1) {
+        if ((n & 1) == 1) {
             // 与运算代替 exponent % 2 == 1
-            result *= base;
+            result *= x;
         }
         return result;
     }
 
     /**
      * 非递归
-     * @param base
-     * @param exponent
+     * @param x
+     * @param n
      * @return
      */
-    private double PowerWithUnsigned2(double base, long exponent) {
+    private double PowerWithUnsigned2(double x, long n) {
         double result = 1;
-        while (exponent != 0) {
-            if ((exponent & 1) == 1) {
-                result *= base;
+        while (n != 0) {
+            if ((n & 1) == 1) {
+                result *= x;
             }
-            base *= base;
-            exponent >>= 1;
+            x *= x;
+            n >>= 1;
         }
         return result;
     }
@@ -81,9 +66,11 @@ public class 求x的n次方 {
 //        System.out.println(obj.myPow(2, 10));
 //        System.out.println(obj.myPow(2.1, 3));
 //        System.out.println(obj.myPow2(2, -2));
-//        System.out.println(obj.myPow2(1.0, -2147483648));
-        System.out.println(obj.myPow2(0.4321, 20));
+        System.out.println(obj.myPow(1.0, -2147483648));
+//        System.out.println(obj.myPow2(0.4321, 20));
 
+
+//        System.out.println(3 & 1);
 //        System.out.println(myPow(0, -1));
 //        System.out.println(Integer.MAX_VALUE);
 //        System.out.println(Integer.MIN_VALUE);
